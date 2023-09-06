67b597efc35942c6d1738f5244aa256a382d7507
Nita Rose <rose_nita@outllok.com>
2023-09-06 11:32:44 -0400

    Updated Music-Library-Frontend file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Music_Library_Frontend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Models.Song> Songs { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Music_Library_Frontend.Models
+{
+    public class Song
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+    }
+}
